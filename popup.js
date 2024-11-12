3881ea04d31731acd566867c29908855b4709778
nazifishrak <nzfishrak60@gmail.com>
2024-11-12 09:51:17 -0800

    Added hardcoded keywords to exclude for MVP

---
+++ popup.js
@@ -0,0 +1,20 @@
+
+const DEFAULT_CATEGORIES = {
+    entertainment: {
+      enabled: false,
+      keywords: ["movie", "music", "trailer", "comedy", "show"]
+    },
+    politics: {
+      enabled: false,
+      keywords: ["election", "senate", "government", "policy", "debate"]
+    },
+    technology: {
+      enabled: false,
+      keywords: ["tech", "gadgets", "review", "software", "hardware"]
+    },
+    sports: {
+      enabled: false,
+      keywords: ["football", "basketball", "soccer", "cricket", "tennis"]
+    }
+  };
+
